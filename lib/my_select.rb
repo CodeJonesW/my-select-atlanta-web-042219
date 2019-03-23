9988699e42d5eeac973dba0ba4b3f8fb5ed66146
Will Jones <willjones2010@gmail.com>
2019-03-23 19:20:21 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select (collection)
   i = 0
   result = []
   while i < collection.length
-   if yield collection[i].even?
+   if yield collection[i] == true
      result.push(collection[i])
    end
     i += 1
