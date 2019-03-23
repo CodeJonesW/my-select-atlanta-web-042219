6e3703fcbb6d499fb505d976fd9d39dfca74946e
Will Jones <willjones2010@gmail.com>
2019-03-23 19:22:04 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,23 @@
-def my_select (collection)
-  i = 0
-  result = []
-  while i < collection.length
-   yield result.push(collection[i])
-    i += 1
+# def my_select (collection)
+#   i = 0
+#   result = []
+#   while i < collection.length
+#   yield result.push(collection[i])
+#     i += 1
+# end
+# collection
+
+
+
+def my_select(array) 
+count = 0
+result = []
+
+while count < array.size
+  if yield(array[count]) == true
+  result.push(array[count])
 end
-collection
+  count+=1
+end
+result
 end
